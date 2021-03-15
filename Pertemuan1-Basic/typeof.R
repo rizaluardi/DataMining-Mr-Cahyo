926b1074b178f5e4d4638524f07abebe9d8e8c90
rizaluardi <rizaluardi@gmail.com>
2021-03-15 14:50:41 +0700

    Tambah Project R
    R VsCode Part1-2

---
+++ Pertemuan1-Basic/typeof.R
@@ -0,0 +1,8 @@
+a <- 100.534564
+b <- as.integer(a)
+
+typeof(a)
+typeof(b)
+
+print(a)
+print(b)
---
+++ Pertemuan1-Basic/typeof_convert.R
@@ -0,0 +1,13 @@
+a <- 100.534564
+b <- as.integer(a)
+
+typeof(a)
+typeof(b)
+
+print(a)
+print(b)
+
+c <- as.double(b)
+
+typeof(c)
+print(c)
